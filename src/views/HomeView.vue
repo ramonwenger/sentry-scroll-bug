6cc5c794544de7aacc91c0472afafbf07c1156c4
Ramon Wenger <ramon.wenger@iterativ.ch>
2025-03-06 16:35:39 +0100

    Add initial files

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1>A very long Page</h1>
+
+    <div class="long"></div>
+    <router-link :to="{ name: 'about' }">Other Page Link</router-link>
+  </main>
+</template>
+
+<style lang="postcss">
+.long {
+  height: 2000px;
+}
+</style>
